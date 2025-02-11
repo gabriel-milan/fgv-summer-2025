942ad0fd8c3d10b6ceef65bbcac8772dd69e30d9
ThiagoTrabach <thiago.trabach@gmail.com>
2025-02-11 16:20:31 -0300

    test: remove incomplete data test for monolito and refactoring models

--- aula03_04/data-tests/assert_sum_monolito_equal_refactoring.sql
+++
@@ -1,9 +0,0 @@
--- check if the sum of the monolito is equal to the refactoring
-
-with monolito as (
-    select * from {{ ref('monolito') }}
-),
-
-refactoring as (
-    select * from {{ ref('refactoring') }}
-)
